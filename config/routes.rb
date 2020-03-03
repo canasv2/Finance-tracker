8f7edb5dce4c7e99701baf1573042b6142de6615
canasv <julio.paniagua96@gmail.com>
2020-03-03 15:50:15 -0600

    friends added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   devise_for :users
   root 'welcome#index'
   get 'search_stock', to: 'stocks#search'   
