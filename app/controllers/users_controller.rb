8fd475138ec234a62cac92cf0c4bb1aba33d7c8b
canasv <julio.paniagua96@gmail.com>
2020-03-05 11:05:10 -0600

    display user/friend profile. END OF COURSE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
+    @user = current_user
   end
   
   def my_friends
     @my_friends = current_user.friends
   end
   
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end    
   
   def search    
     if params[:friend].present?      
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'search_stock', to: 'stocks#search'
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
